11f543fc4a4a43aa317b2f3da48d908c05f1febe
Giammaria Biffi <gmb2005itabackup@gmail.com>
2023-01-26 19:23:12 +0100

    Fixed UserInterface class

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 * La classe UserInterface definisce l'interfaccia utente del programma, rendendo possibile all'utente interagire con
  * esso.
  * @author Giammaria Biffi
- * @version 0.5
+ * @version 1
  * */
 public class UserInterface {
 	private Garage garage;
@@ -55,5 +55,34 @@ public class UserInterface {
 		//come faccio a dire "Per ogni veicolo presente nella lista posti occupati?"
 	}
 
-	private void farEntrareVeicolo(){}
+	/**
+*
+ * Metodo per far entrare un veicolo nel Garage
+ * @author Giammaria Biffi*/
+	public void farEntrareVeicolo() {
+		System.out.println("Inserire il tipo di veicolo (1 per Automobile, 2 per Motocicletta): ");
+		int scelta = scanner.nextInt();
+		if (scelta == 1) {
+			System.out.println("Inserire la targa dell'automobile: ");
+			String targa=scanner.nextLine();
+			System.out.println("Inserire il modello dell'auto: ");
+			String modello=scanner.nextLine();
+			System.out.println("Per ragioni logistiche, inserire anche il numero di porte di cui dispoone il modello:");
+			int numPorte=scanner.nextInt();
+			Automobile auto = new Automobile(targa, modello, numPorte);
+			garage.entraVeicolo(auto);
+		} else if (scelta == 2) {
+			System.out.println("Inserire la targa della motocicletta: ");
+			String targa=scanner.nextLine();
+			System.out.println("Inserire il modello della motocicletta: ");
+			String modello=scanner.nextLine();
+			System.out.println("Per ragioni logistiche, inserire anche la cilindrata del modello: ");
+			int cilindrata=scanner.nextInt();
+			Motocicletta moto = new Motocicletta(targa, modello, cilindrata);
+			garage.entraVeicolo(moto);
+		} else {
+			System.out.println("Scelta non valida. Riprovare.");
+		}
+	}
+
 }
